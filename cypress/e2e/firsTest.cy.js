ddb843118db52d25c9a4ab5a4593fa8bd940d113
Sangeetha Easwaran <sangeethaeaswaran01@gmail.com>
2024-05-25 18:00:31 +0100

    table example

--- cypress/e2e/firsTest.cy.js
+++ cypress/e2e/firsTest.cy.js
@@ -1,12 +1,13 @@
 //<references types="Cypress">
 describe('Test suite', ()=>{
-    it.only('Test case', ()=>{
+    it('Test case', ()=>{
         // cy.visit('https://testautomationpractice.blogspot.com/');
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         cy.get('[name="username"]').type('Admin')
         cy.get('[name="password"]').type('admin123')
         cy.get('[type="submit"]').click()
         cy.get('h6').should('have.text', 'Dashboard')
+
         cy.get('[class="oxd-topbar-header-title"]').click()
         cy.get('[class="oxd-sidepanel"]')
         cy.get('li').find('span').contains('Leave').click()
@@ -19,20 +20,106 @@ describe('Test suite', ()=>{
         .click()
         // cy.get('[class="orangehrm-card-container"]').contains('Apply Leave')
        
-        //cypress alias
+        //cypress alias-global in this page 
         cy.get('[class="orangehrm-card-container"]').as('card')
         cy.get('@card').find('h6').should('contain', 'Apply Leave')
         cy.get('@card').find('label').should('contain', 'Leave Type')
         cy.get('@card').find('[class="oxd-select-wrapper"]').click()
 
+        //cypress then 
+        cy.get('[class="orangehrm-card-container"]').then(card =>{
+            cy.wrap(card).find('label').contains('Leave Balance')
+            cy.wrap(card).find('label').contains('From Date')
+        })
 
-// Leave Type
+
     })
+
+    //radio button example 
     it('Second test', ()=>{
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.contains('Login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+        cy.get('h6').should('have.text', 'Dashboard') 
+        // cy.get('oxd-userdropdown-tab').find('span').click()
+        // cy.get('oxd-dropdown-menu').find('li').find('a').should('contain', 'About')
         
+        cy.get('[class="oxd-topbar-header-title"]').click()
+        cy.get('[class="oxd-sidepanel"]')
+        cy.get('li').find('span').contains('My Info').click()
+
+        cy.contains('Personal Details').click()
+        cy.get('label').contains('Gender');
+
+        cy.get('[type="radio"]').as('radioButtons')
+        cy.get('@radioButtons').eq(0).check({force: true}).should('be.checked')
+        cy.get('@radioButtons').eq(1).check({force: true}).should('be.checked')
+        cy.get('@radioButtons').eq(0).should('not.be.checked')
+    })
+
+    //checkbox example 
+    it('Checkbox', ()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+        cy.get('h6').should('have.text', 'Dashboard') 
+
+        cy.get('[class="oxd-topbar-header-title"]').click()
+        cy.get('[class="oxd-sidepanel"]')
+        cy.get('li').find('span').contains('PIM').click()
+
+        cy.get('a').contains('Reports').click()
+        cy.get('[type="checkbox"]').as('checkboxes')
+        cy.get('@checkboxes').eq(3).check({force: true}).should('be.checked')
+        cy.get('@checkboxes').uncheck({force:true})
+    })
+
+    //date picker 
+    it('Date Picker', ()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+        cy.get('h6').should('have.text', 'Dashboard') 
+
+        cy.get('[class="oxd-topbar-header-title"]').click()
+        cy.get('[class="oxd-sidepanel"]')
+        cy.get('li').find('span').contains('Time').click()
+        cy.get('[class="oxd-topbar-body-nav-tab-item"]').contains("Attendance").click()
+        cy.get('[class="oxd-topbar-body-nav-tab-link"]').contains("My Records").click()
+        cy.get('[class="oxd-icon bi-calendar oxd-date-input-icon"]').click()
+
+
+    })
+
+    it('Dropdown list', ()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+        cy.get('h6').should('have.text', 'Dashboard') 
+
+        cy.get('[class="oxd-topbar-header-title"]').click()
+        cy.get('[class="oxd-sidepanel"]')
+        cy.get('li').find('span').contains('Time').click()
+        cy.get('[class="oxd-topbar-body-nav-tab --parent"]').contains("Reports").as('dropDown')
+        cy.get('@dropDown').click()
+        cy.get('[class="oxd-topbar-body-nav-tab-link"]').as('dropDownList')
+       
+        cy.get('@dropDownList').each((listItem, index)=> {
+                // const text = listItem.text().trim();
+                // cy.log('text', text, index)
+                cy.wrap(listItem).click()
+                // cy.get('@listItem').click()
+            })
+            // cy.get('@dropDown')
+            cy.get('@dropDown').click()
+            })
+    it.only('Table example', ()=>{
+                cy.visit('https://example.cypress.io/commands/traversal')
+                cy.get('.table.traversal-table td').as('t').first().should('contain', 1)
+                cy.get('t').last().should('contain', 2)
     })
 })
-
-    // expect(true).to.equal(true);
